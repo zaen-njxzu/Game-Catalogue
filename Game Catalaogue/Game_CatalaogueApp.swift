ff5d4d05ae6d0d4edd57e2f0ab0156be20516410
zaenal <zaenal@laku6.com>
2021-01-10 22:29:21 +0700

    initial commit

---
+++ Game Catalaogue/Game_CatalaogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_CatalaogueApp.swift
+//  Game Catalaogue
+//
+//  Created by Zaenal Arsy on 10/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_CatalaogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
